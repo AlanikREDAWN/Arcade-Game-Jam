ead927afe5f44b49c3baf281e445285af7bd247f
Alanik <lilia.dostie@socprep.org>
2024-08-12 15:54:45 -0400

    continued working on my little slime guy!

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let slime;
+let slime, floor;
 
 function preload() {
 	slimeSheet = loadImage('slimeSheet.png');
@@ -11,15 +11,25 @@ function setup() {
 	slime.diameter = 64;
 	slime.spriteSheet = slimeSheet;
 	slime.friction = 0;
-
+	
 	slime.addAnis({
 		left: { row: 0, frameSize: [64, 64], frames: 8 },
 		right: { row: 1, frameSize: [64, 64], frames: 8 },
-		up: { row: 2, frameSize: [64, 64], frames: 1 },
-		idle: { row: 2, frameSize: [64, 64], frames: 8 },
+		idle: { row: 2, frameSize: [64, 64], frames: 8  },
 	});
 	slime.changeAni('idle');
+	
+	floor = new Sprite();
+	floor.y = 400;
+	floor.w = 500;
+	floor.h = 50;
+	floor.collider = 'static';
+	floor.stroke = '#3B8E7A'
+	floor.color = '#00C2AE';
+	floor.strokeWeight = 5;
 
+
+  world.gravity.y = 10;
 }
 
 function draw() {
@@ -31,14 +41,26 @@ function draw() {
 	// 	slime.speed = 10;
 	// 	slime.moveTo(mouse);
 	// }
-	if (kb.presses('left') || kb.presses('a')) {
+	
+	if (kb.pressing('left') || kb.pressing('a')) {
 		slime.changeAni('left');
 		slime.vel.x = -2;
-	} else if (kb.presses('right') || kb.presses('d')) {
+	} else if (kb.pressing('right') || kb.pressing('d')) {
 		slime.changeAni('right');
 		slime.vel.x = 2;
-	} else if (kb.presses('up') || kb.presses('w')) {
-		slime.chnagh
+	} else if (kb.presses('up') || kb.presses('w') || kb.presses('space')){
+		slime.vel.y = -3;
+	} else {
+		slime.changeAni('idle');
+		slime.vel.x = 0;
 	}
+	// } else if (kb.presses('up') || kb.presses('w') || kb.presses('space')) {
+	// 	slime.vel.y = 9;
+	// }
+	// } else {
+	// 	// slime.changeAni('idle'); 
+	// 	// slime.vel.y = 0; 
+	// 	// slime.vel.x = 0;
+	// }
 
 }
